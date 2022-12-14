1f16d23ceebe5ea18f48ca08fd025930a18dfe68
Daahir Ibraahim <Daahir.ibraahim89@gmail.com>
2022-12-14 17:44:22 +0300

    update js

--- blog-client-template/admin/js/manage-posts.js
+++ blog-client-template/admin/js/manage-posts.js
@@ -1,1 +1,12 @@
 const tbody = document.getElementById("tbody");
+fetchAllPosts();
+
+async function fetchAllPosts() {
+  try {
+    const response = await fetch(
+      "https://blog-api-assignment.up.railway.app/posts"
+    );
+    }
+
+// let posts = await response.json();
+}
