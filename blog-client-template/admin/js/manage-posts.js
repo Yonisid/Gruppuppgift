c7a55e3992c5808596b80d006f9953c59b36557a
Daahir Ibraahim <Daahir.ibraahim89@gmail.com>
2022-12-15 12:57:20 +0300

    update table

--- blog-client-template/admin/js/manage-posts.js
+++ blog-client-template/admin/js/manage-posts.js
@@ -15,14 +15,31 @@ async function fetchAllPosts() {
     for (let post of post) {
       let postDate = new Date(post.date);
 
-    //   <tr>
-    //   <th>Title</th>
-    //   <th>Author</th>
-    //   <th>Tags</th>
-    //   <th>Date</th>
-    //   <th>Action</th>
-    // </tr>
+      fetchAllPosts();
 
+      async function fetchAllPosts() {
+        try {
+          const response = await fetch(
+            "https://blog-api-assignment.up.railway.app/posts"
+          );
+          let posts = await response.json();
+      
+          let trContent = "";
+      
+          for (let post of posts) {
+            let postDate = new Date(post.date);
+      
+            trContent += `<tr>
+                      <th>${post.title} </th>
+                      <th>${post.author}</th>
+                      <th>${post.tags}</th>
+                      <th>${postDate.getFullYear()}-
+                      ${postDate.getMonth()}-${postDate.getDate()}</th> `;
+          }
+        }
+      }
+    }
   }
 }
-}
+
+    
