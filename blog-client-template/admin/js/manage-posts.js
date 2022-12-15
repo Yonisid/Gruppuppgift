1a0f7493b0b0f25a93aa07f1b20bdf24707fd735
Daahir Ibraahim <Daahir.ibraahim89@gmail.com>
2022-12-15 14:25:58 +0300

    fix table

--- blog-client-template/admin/js/manage-posts.js
+++ blog-client-template/admin/js/manage-posts.js
@@ -34,12 +34,15 @@ async function fetchAllPosts() {
                       <th>${post.author}</th>
                       <th>${post.tags}</th>
                       <th>${postDate.getFullYear()}-
-                      ${postDate.getMonth()}-${postDate.getDate()}</th> `;
+                      ${postDate.getMonth() + 1}-${postDate.getDate()}</th> 
+                      <th><a id="update" href="#">Update</a> |
+                      <a id="delete" href="#">Delete</a> </th></tr>
+                         
+                  `;
           }
+      
+          tbody.innerHTML += trContent;
+        } catch (error) {
+          console.log(error);
         }
       }
-    }
-  }
-}
-
-    
