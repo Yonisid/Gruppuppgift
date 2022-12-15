746f05023a35fbc5f5f283fd13f5fd3ce54a0993
Daahir Ibraahim <Daahir.ibraahim89@gmail.com>
2022-12-15 14:59:50 +0300

    Done

--- blog-client-template/admin/js/manage-posts.js
+++ blog-client-template/admin/js/manage-posts.js
@@ -1,48 +1,33 @@
 const tbody = document.getElementById("tbody");
 
 fetchAllPosts();
+
 async function fetchAllPosts() {
   try {
     const response = await fetch(
       "https://blog-api-assignment.up.railway.app/posts"
     );
-
-
     let posts = await response.json();
 
     let trContent = "";
 
-    for (let post of post) {
+    for (let post of posts) {
       let postDate = new Date(post.date);
 
-      fetchAllPosts();
+      trContent += `<tr>
+                <th>${post.title} </th>
+                <th>${post.author}</th>
+                <th>${post.tags}</th>
+                <th>${postDate.getFullYear()}-
+                ${postDate.getMonth() + 1}-${postDate.getDate()}</th>
+                <th><a id="update" href="#">Update</a> |
+                <a id="delete" href="#">Delete</a> </th></tr>
+                   
+            `;
+    }
 
-      async function fetchAllPosts() {
-        try {
-          const response = await fetch(
-            "https://blog-api-assignment.up.railway.app/posts"
-          );
-          let posts = await response.json();
-      
-          let trContent = "";
-      
-          for (let post of posts) {
-            let postDate = new Date(post.date);
-      
-            trContent += `<tr>
-                      <th>${post.title} </th>
-                      <th>${post.author}</th>
-                      <th>${post.tags}</th>
-                      <th>${postDate.getFullYear()}-
-                      ${postDate.getMonth() + 1}-${postDate.getDate()}</th> 
-                      <th><a id="update" href="#">Update</a> |
-                      <a id="delete" href="#">Delete</a> </th></tr>
-                         
-                  `;
-          }
-      
-          tbody.innerHTML += trContent;
-        } catch (error) {
-          console.log(error);
-        }
-      }
+    tbody.innerHTML += trContent;
+  } catch (error) {
+    console.log(error);
+  }
+}
